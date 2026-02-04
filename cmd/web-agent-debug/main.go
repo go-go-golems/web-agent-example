36d3bfe92e0891524e4c75b3ac3043ec0b50f3df
Manuel Odendahl <wesen@ruinwesen.com>
2026-02-04 18:32:27 -0500

    web-agent-debug: add CLI skeleton

---
+++ cmd/web-agent-debug/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(2)
+	}
+
+	switch os.Args[1] {
+	case "chat", "ws", "timeline", "run":
+		if err := runCommand(os.Args[1], os.Args[2:]); err != nil {
+			fmt.Fprintln(os.Stderr, "error:", err)
+			os.Exit(1)
+		}
+	default:
+		usage()
+		os.Exit(2)
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "web-agent-debug: CLI harness for webchat (/chat + /ws)\n\n")
+	fmt.Fprintf(os.Stderr, "Usage:\n")
+	fmt.Fprintf(os.Stderr, "  web-agent-debug <chat|ws|timeline|run> [flags]\n\n")
+}
+
+func runCommand(cmd string, args []string) error {
+	fs := flag.NewFlagSet(cmd, flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+	return fmt.Errorf("command %s not implemented yet", cmd)
+}
